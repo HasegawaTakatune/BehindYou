c655180c8d15306fbe9072b1fa94abfb9936b26f
HasegawaTakatune <0ta5274738t4p4r@gmail.com>
2022-04-03 13:12:52 +0900

    title: タイトル・設定の画面遷移
    message: タイトル・設定の画面遷移、最初から・ロードボタンの仮実装

--- Assets/Scripts/TitleConfig/TitleController.cs
+++ Assets/Scripts/TitleConfig/TitleController.cs
@@ -5,28 +5,34 @@ public class TitleController : MonoBehaviour
     [SerializeField] private GameObject titlePanel = default;
     [SerializeField] private GameObject configPanel = default;
 
-    void Start()
+    public void onStartGame()
     {
-
+        Debug.Log("Game start");
     }
 
-    void onStartGame()
+    public void onLoadGame()
     {
-
+        Debug.Log("Game load");
     }
 
-    void onLoadGame()
-    {
-
-    }
-
-    void onConfigOpen()
+    public void onConfigOpen()
     {
         configPanel.SetActive(true);
+        titlePanel.SetActive(false);
     }
 
-    void onConfigClose()
+    public void onConfigClose()
     {
+        titlePanel.SetActive(true);
         configPanel.SetActive(false);
+    }
+
+    public void onExitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
     }
 }
